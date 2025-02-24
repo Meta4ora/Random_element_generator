463789d0389a70bd24a4de09737f20714acb809a
Denis Fokin <fokindenlelush2006@gmail.com>
2025-02-25 01:59:40 +0300

    Update print format (Make it more better and aesthetic).

--- Random_element_generator/Random_element_generator.py
+++ Random_element_generator/Random_element_generator.py
@@ -1,16 +1,14 @@
 import random
 
 def generate_random_numbers(count):
-    #Генерирует список случайных чисел в диапазоне от 5 до 1600. В параметрах передаем число сгенерированных чисел
-    numbers = [random.randint(5, 16 * 100) for x in range(count)]
-    return numbers
+    # Генерирует список случайных чисел в диапазоне от 5 до 1600.
+    return [random.randint(5, 16 * 100) for _ in range(count)]
 
 if __name__ == "__main__":
-    number_of_elements = 16 + 10  # 26 элементов (10+16)
+    number_of_elements = 16 + 10  # 26 элементов (10 + 16)
     random_numbers = generate_random_numbers(number_of_elements)
     
     # Вывод сгенерированных чисел
     print("Сгенерированные случайные числа:")
-    for number in random_numbers:
-        print(number)
-
+    for i, number in enumerate(random_numbers, start=1):
+        print(f"Число {i}: {number}")
