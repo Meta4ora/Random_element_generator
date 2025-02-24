2271f12e18883a0b332d03085b28a2e0bb493d73
Denis Fokin <fokindenlelush2006@gmail.com>
2025-02-25 01:47:36 +0300

    Добавьте файлы проекта.

---
+++ Random_element_generator/Random_element_generator.py
@@ -0,0 +1,15 @@
+import random
+
+def generate_random_numbers(count):
+    #Генерирует список случайных чисел в диапазоне от 5 до 1600.
+    return"Пока в разработке!"
+
+if __name__ == "__main__":
+    number_of_elements = 16 + 10  # 26 элементов
+    random_numbers = generate_random_numbers(number_of_elements)
+    
+    # Вывод сгенерированных чисел
+    print("Сгенерированные случайные числа:")
+    for number in random_numbers:
+        print(number)
+
