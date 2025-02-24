251a8f5765236c4082ec1ca639e97296af91c765
Denis Fokin <fokindenlelush2006@gmail.com>
2025-02-25 01:51:20 +0300

    Added functionality for empty method. Program is complete!

--- Random_element_generator/Random_element_generator.py
+++ Random_element_generator/Random_element_generator.py
@@ -1,8 +1,9 @@
 import random
 
 def generate_random_numbers(count):
-    #Генерирует список случайных чисел в диапазоне от 5 до 1600.
-    return"Пока в разработке!"
+    #Генерирует список случайных чисел в диапазоне от 5 до 1600. В параметрах передаем число сгенерированных чисел
+    numbers = [random.randint(5, 16 * 100) for x in range(count)]
+    return numbers
 
 if __name__ == "__main__":
     number_of_elements = 16 + 10  # 26 элементов (10+16)
